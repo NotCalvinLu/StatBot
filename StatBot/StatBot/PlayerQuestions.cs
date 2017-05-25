66823430642d334c21410e3376d1ca8000ccc58b
Matthew Nordstrand <matt_nordstrand@live.com>
2017-05-25 19:03:56 -0400

    Started PlayerQuestions class.

---
+++ StatBot/StatBot/PlayerQuestions.cs
@@ -0,0 +1,61 @@
+﻿using Discord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatBot
+{
+    class PlayerQuestions
+    {
+        public enum CharClass { BERSERKER, DARK_KNIGHT, KUNOICHI, MAEHWA, MUSA, NINJA, RANGER, SORCERESS, TAMER, VALKYRIE, WARRIOR, WITCH, WIZARD }
+
+        int curQuestion = 0;
+        User user;
+
+        //Information for the bot to collect on the user:
+        ulong userID;
+        string discordName;
+        string familyName;
+        string charName;
+        CharClass charClass;
+        int level;
+        int ap;
+        int awakenAp;
+        int dp;
+        string screenshotUrl;
+        string nextUpgrade;
+        string nextLevel;
+        string otherInfo;
+        /////////////////////////////////////////////////
+
+        public PlayerQuestions(User user)
+        {
+            this.user = user;
+            userID = user.Id;
+            discordName = user.Nickname;
+
+            if (discordName == null || discordName.Equals(""))
+            {
+                discordName = user.Name;
+            }
+
+            askQuestion();
+        }
+
+        public void askQuestion()
+        {
+            string question = "";
+
+            switch (curQuestion)
+            {
+                case 0:
+                    //TODO: Add actual questions.
+                    break;
+            }
+
+            user.SendMessage(question);
+        }
+    }
+}
--- StatBot/StatBot/StatBot.cs
+++ StatBot/StatBot/StatBot.cs
@@ -1,11 +1,10 @@
-﻿using System;
+﻿using Discord;
+using Discord.Commands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-using Discord;
-using Discord.Commands;
 
 namespace StatBot
 {
